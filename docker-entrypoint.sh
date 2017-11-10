8a58258e5422bde229261ea436280721efb6bb3d
Lucas Chain <sou@lucaschain.com.br>
2017-11-10 15:48:58 -0200

    migration command to migrate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 COMMAND=$1
 
-if [[ "${COMMAND}" == "migration" ]]; then
+if [[ "${COMMAND}" == "migrate" ]]; then
   echo "Migration completed"
 else
   supervisord --nodaemon --configuration /etc/supervisord.conf
