70a88b9e5a20e810e2271c27d892b634269165cf
Lucas Chain <sou@lucaschain.com.br>
2017-11-10 10:16:26 -0200

    dummy migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,6 @@
 
 COMMAND=$1
 
-if [[ -z $COMMAND ]]; then
-  echo "Please specify which command to run ('app' or 'migration')"
-  exit 1
-fi
-
 if [[ "${COMMAND}" == "migration" ]]; then
   echo "Migration completed"
 else
